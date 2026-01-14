25ce654f146c8f444408e0b877c56aa61d5f5040
Kushagra <Kushagra.mac@gmail.com>
2026-01-14 16:24:09 +0530

    Fix typescript error for Vercel

--- src/bot-final.ts
+++ src/bot-final.ts
@@ -55,6 +55,7 @@ discord.on('messageCreate', async (message: Message) => {
   if (message.author.bot || !message.content) return;
 
   try {
+    // @ts-ignore
     await message.channel.sendTyping();
 
     // A. Save User Message
